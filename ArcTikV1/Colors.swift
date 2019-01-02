9de59c9f6395569c27328f49b1c78ee4d6f7c462
Brandon <fllawlezz@gmail.com>
2019-01-02 00:15:01 -0800

    Commit 1, setup

---
+++ ArcTikV1/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  ArcTikV1
+//
+//  Created by Brandon In on 1/2/19.
+//  Copyright © 2019 Brandon In. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    
+    static let appBlue = UIColor(red: 0/255, green: 153/255, blue: 212/255, alpha: 1);
+    static let lightAppBlue = UIColor(red: 0/255, green: 183/255, blue: 255/255, alpha: 1);
+    static let veryLightGray = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0);
+    static let superLightGray = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1.0);
+    
+}
---
+++ ArcTikV1/LoginPage.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPage.swift
+//  ArcTikV1
+//
+//  Created by Brandon In on 1/1/19.
+//  Copyright © 2019 Brandon In. All rights reserved.
+//
+
+import UIKit
+
+class LoginPage: UIViewController{
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
